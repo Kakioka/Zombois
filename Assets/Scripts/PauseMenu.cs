b6e596f2f50ca26ce4d07900fb086e4ab3f5311b
Kuliakaiwikuamoo Akioka <kakioka@hawaii.edu>
2023-02-05 23:45:22 -1000

    Pause Menu
    It's all in this branch. To use it, just drag the "PauseCanvas" object into your scene and drag the necessary game objects to the needed locations.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    //Update is called once per fram
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("Kaiwi");
+
+    }
+
+    public void QuitGame(){
+
+        Debug.Log("Quitting game...");
+        Application.Quit();
+
+    }
+
+}
