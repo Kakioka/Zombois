e9a9396507d91f8ad287141ebbedbeaed3341663
Cris Javier <crisj@hawaii.edu>
2023-02-21 20:37:05 -1000

    update

--- Assets/Scripts/HPManager.cs
+++ Assets/Scripts/HPManager.cs
@@ -2,46 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class HPManager : MonoBehaviour
 {
     public int health = 3;
-    
-    public float moveSpeed = 5f;
-
-    public Rigidbody2D rb;
-
-    Vector2 movement;
-    Vector2 mousePos;
-
-    public Camera cam;
-
     public bool isDead = false;
-
     public Animator ani;
-
-    public GameObject player;
-
-    public GameObject health3;
-
-    public GameObject health2;
-    
-    public GameObject health1;
 
     // Start is called before the first frame update
     void Start()
     {
         ani = GetComponent<Animator>();
-        ani.SetBool("Dead", true);   
+        ani.SetBool("Dead", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
         if (health == 2)
         {
             ani.SetBool("IsHitOnce", true);
@@ -63,19 +39,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.Log("dead");
             isDead = true;
-            player.SetActive(false);
             //health1.SetActive(false);
         }
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        Vector2 lookDir = mousePos - rb.position;
-
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
-
     }
 }
