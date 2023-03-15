19925959e415386b8038c0968357b4c74eba19f5
JustinY <jyip808@hawaii.edu>
2023-03-14 17:12:06 -1000

    update shop

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -6,6 +6,9 @@ public class ItemManager : MonoBehaviour
 {
     public List<GameObject> Items = new List<GameObject>();
 
+    private GameObject player;
+    private GameObject gameM;
+
     public Transform item1;
     public Transform item2;
     public Transform item3;
@@ -16,12 +19,32 @@ public class ItemManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(Items[Random.Range(0, Items.Count)], item1);
-        Instantiate(Items[Random.Range(0, Items.Count)], item2);
-        Instantiate(Items[Random.Range(0, Items.Count)], item3);
-        Instantiate(Items[Random.Range(0, Items.Count)], item4);
-        Instantiate(Items[Random.Range(0, Items.Count)], item5);
-        Instantiate(Items[Random.Range(0, Items.Count)], item6);
+        player = gameObject.GetComponent<upgradeShopManager>().player;
+        gameM = gameObject.GetComponent<upgradeShopManager>().gameManager;
+
+        GameObject item = Instantiate(Items[Random.Range(0, Items.Count)], item1);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
+
+        item = Instantiate(Items[Random.Range(0, Items.Count)], item2);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
+
+        item = Instantiate(Items[Random.Range(0, Items.Count)], item3);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
+
+        item = Instantiate(Items[Random.Range(0, Items.Count)], item4);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
+
+        item = Instantiate(Items[Random.Range(0, Items.Count)], item5);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
+
+        item = Instantiate(Items[Random.Range(0, Items.Count)], item6);
+        item.GetComponent<Item>().player = player;
+        item.GetComponent<Item>().gameM = gameM;
     }
 
     // Update is called once per frame
